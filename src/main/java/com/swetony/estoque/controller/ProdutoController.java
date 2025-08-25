ad219df1c1f1c95f1805bcb7db1df7288a9b8951
Swetony <swetonyancelmo@gmail.com>
2025-08-25 20:40:24 -0300

    alterando requestmapping

--- src/main/java/com/swetony/estoque/controller/ProdutoController.java
+++ src/main/java/com/swetony/estoque/controller/ProdutoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/produto")
+@RequestMapping(value = "/produtos")
 public class ProdutoController {
 
     @Autowired
