385833c2dfa24abfac804b4c72931b20d35e3267
Swetony <swetonyancelmo@gmail.com>
2025-08-29 13:06:14 -0300

    Atualizando controller para usar dto

--- src/main/java/com/swetony/produto/controller/ProdutoController.java
+++ src/main/java/com/swetony/produto/controller/ProdutoController.java
@@ -1,9 +1,12 @@
 package com.swetony.produto.controller;
 
+import com.swetony.produto.dto.ProdutoRequestDTO;
+import com.swetony.produto.dto.ProdutoResponseDTO;
 import com.swetony.produto.entity.Produto;
 import com.swetony.produto.service.ProdutoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,36 +19,38 @@ public class ProdutoController {
     @Autowired
     private ProdutoService service;
 
-    @PostMapping("/criar")
-    public ResponseEntity<Produto> criarProduto(@RequestBody @Valid Produto produto){
-        produto = service.save(produto);
-        return ResponseEntity.ok().body(produto);
+    @PostMapping()
+    public ResponseEntity<ProdutoResponseDTO> criarProduto(@RequestBody @Valid ProdutoRequestDTO produtoDTO){
+        ProdutoResponseDTO produtoSalvo = service.save(produtoDTO);
+
+        return new ResponseEntity<>(produtoSalvo, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Produto> atualizarProduto(@PathVariable Long id, @RequestBody @Valid Produto produto){
-        Produto atualizado = service.updateById(id, produto);
-        if(atualizado == null){
+    public ResponseEntity<ProdutoResponseDTO> atualizarProduto(@PathVariable Long id,
+                                                               @RequestBody ProdutoRequestDTO produtoDTO){
+        ProdutoResponseDTO produtoAtualizado = service.updateById(id, produtoDTO);
+        if(produtoAtualizado == null){
             return ResponseEntity.notFound().build();
         }
-        return ResponseEntity.ok().body(atualizado);
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Produto> buscarProdutoPorId(@PathVariable Long id){
-        Produto produto = service.findById(id);
-        return ResponseEntity.ok().body(produto);
+        return ResponseEntity.ok().body(produtoAtualizado);
     }
 
     @GetMapping
-    public ResponseEntity<List<Produto>> buscarProdutos(){
-        List<Produto> produtos = service.findAll();
-        return ResponseEntity.ok().body(produtos);
+    public ResponseEntity<List<ProdutoResponseDTO>> buscarProdutos(){
+        List<ProdutoResponseDTO> produtos = service.findAll();
+        return ResponseEntity.ok(produtos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProdutoResponseDTO> buscarProdutoPorId(@PathVariable Long id){
+        ProdutoResponseDTO produto = service.findById(id);
+        return ResponseEntity.ok(produto);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Produto> deletarPorId(@PathVariable Long id){
-        Produto produtoExistente = service.findById(id);
+    public ResponseEntity<ProdutoResponseDTO> deletarPorId(@PathVariable Long id){
+        ProdutoResponseDTO produtoExistente = service.findById(id);
         if(produtoExistente == null){
             return ResponseEntity.notFound().build();
         }
